f85685c5a7ac014a7ab58027a1b067fdba679848
oharej <oharej@gmail.com>
2014-03-03 22:48:50 +0000

    Adding app files

---
+++ app/scripts/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+angular.module('courantApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html'
+      })
+      .when('/history', {
+        templateUrl: 'views/history.html'
+      })
+      .when('donations', {
+        templateUrl: 'views/donations.html'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
